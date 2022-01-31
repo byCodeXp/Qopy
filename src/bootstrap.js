9c20ad4f1af4e7e9f89ea1a527a2000a51707568
Dima <bycodexp@gmail.com>
2022-01-31 21:08:37 +0200

    initial electorn.js app

---
+++ src/bootstrap.js
@@ -0,0 +1,12 @@
+const { app, BrowserWindow } = require('electron');
+
+app.whenReady().then(() => {
+
+   const window = new BrowserWindow({
+      width: 800,
+      height: 600
+   });
+
+   window.loadFile('src/index.html');
+
+});
