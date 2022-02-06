376c793fe7d1d5edb5c253f4be1586b9ff52c5d2
Dima <bycodexp@gmail.com>
2022-02-06 03:05:27 +0200

    open, close, switch tabs functionality

--- src/app/components/tab/index.tsx
+++ src/app/components/tab/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { LegacyRef, useRef } from 'react';
 
-const IconClose = () => (
+const IconClose = React.forwardRef<any, any>((props, ref) => (
     <svg
+        ref={ref}
         xmlns="http://www.w3.org/2000/svg"
         className="h-4 w-4"
         fill="none"
@@ -15,13 +16,42 @@ const IconClose = () => (
             d="M6 18L18 6M6 6l12 12"
         />
     </svg>
-);
+));
 
-const Tab = ({ label }: { label: string }) => (
-    <div className="bg-[#333333] w-[200px] h-[34px] px-[12px] flex flex-row justify-between items-center">
-        <span className="leading-[34px] text-white">{label}</span>
-        <IconClose />
-    </div>
-);
+interface Props {
+    label: string;
+    active: boolean;
+    onClick: () => void;
+    onClose: (index: number) => void;
+    index: number;
+}
+
+const Tab = ({ label, active, onClick, onClose, index }: Props) => {
+    const ref = useRef(null);
+
+    const handleClick = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    ) => {
+        if (event.target !== ref.current) {
+            onClick();
+        }
+    };
+
+    return (
+        <div
+            onClick={handleClick}
+            className={`${
+                active
+                    ? 'bg-[#1b1b1b] h-[36px]'
+                    : 'bg-[#333333] mb-[2px] h-[34px]'
+            } w-[200px] px-[12px] flex flex-row justify-between items-center`}
+        >
+            <span className="leading-[34px] text-white">{label}</span>
+            <div onClick={() => onClose(index)}>
+                <IconClose ref={ref} />
+            </div>
+        </div>
+    );
+};
 
 export { Tab };
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { render } from 'react-dom';
 import { Tab } from './components/tab';
 
@@ -6,7 +6,45 @@ import './tailwind.css';
 
 const { ipcRenderer } = window.require('electron');
 
-const App = () => {
+const IconPlus = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-4 w-4"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="#818181"
+    >
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={3}
+            d="M12 4v16m8-8H4"
+        />
+    </svg>
+);
+
+const App: React.FC = () => {
+    const [tabs, setTabs] = useState<{ label: string }[]>([]);
+
+    const [activeTab, setActiveTab] = useState<number>(-1);
+
+    const handleAddTab = () => {
+        setTabs([...tabs, { label: Math.random().toString() }]);
+        setActiveTab(tabs.length);
+    };
+
+    const handleSetActiveTab = (index: number) => {
+        setActiveTab(index);
+    };
+
+    const handleCloseTab = (index: number) => {
+        setTabs([...tabs.slice(0, index), ...tabs.slice(index + 1)]);
+
+        if (tabs.length === 1) {
+            setActiveTab(-1);
+        }
+    };
+
     const handleClickMinimize = () => {
         ipcRenderer.send('WINDOW_MINIMIZE');
     };
@@ -19,13 +57,35 @@ const App = () => {
         ipcRenderer.send('WINDOW_CLOSE');
     };
 
+    useEffect(() => {
+        console.log('Active tab: ' + activeTab);
+    }, [activeTab]);
+
+    useEffect(() => {
+        console.log('Tabs: ' + tabs.length);
+    }, [tabs]);
+
     return (
         <div className="flex flex-col h-full">
             <header className="flex flex-row justify-between drag">
-                <div className="flex flex-row gap-[2px] no-drag py-[2px] px-[2px]">
-                    <Tab label="index.tsx" />
-                    <Tab label="app.tsx" />
-                    <Tab label="test.tsx" />
+                <div className="flex flex-row gap-[2px] no-drag pt-[2px] px-[2px] items-center">
+                    {tabs.map((tab, index) => (
+                        <div key={index}>
+                            <Tab
+                                label={tab.label}
+                                active={index === activeTab}
+                                onClick={() => handleSetActiveTab(index)}
+                                onClose={handleCloseTab}
+                                index={index}
+                            />
+                        </div>
+                    ))}
+                    <div
+                        onClick={handleAddTab}
+                        className="w-[34px] flex justify-center"
+                    >
+                        <IconPlus />
+                    </div>
                 </div>
                 <div className="flex flex-row gap-[8px] no-drag py-[8px] pr-[16px]">
                     <div
@@ -43,10 +103,12 @@ const App = () => {
                 </div>
             </header>
             <main className="px-[2px] pb-[2px] flex-1">
-                <textarea
-                    spellCheck={false}
-                    className="appearance-none resize-none w-full h-full outline-none text-white bg-[#1B1B1B] p-[8px] font-mono text-[14px]"
-                ></textarea>
+                {activeTab !== -1 && (
+                    <textarea
+                        spellCheck={false}
+                        className="appearance-none resize-none w-full h-full outline-none text-white bg-[#1B1B1B] p-[8px] font-mono text-[14px]"
+                    ></textarea>
+                )}
             </main>
         </div>
     );
